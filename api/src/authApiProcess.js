2f488150bb8f453247ba2754b47c26311904310d
ArbazKhan-Developer <kkabz860156@gmail.com>
2023-06-01 14:44:51 +0530

    initial commit

---
+++ api/src/authApiProcess.js
@@ -0,0 +1,15 @@
+class authApiProcess {
+
+
+    async process(event){
+        console.log(`event received: ${JSON.stringify(event)}`);
+
+        return {
+            statuCode: 200,
+            message: 'api processed successfully'
+        }
+    }
+}
+
+
+module.exports = new authApiProcess()
