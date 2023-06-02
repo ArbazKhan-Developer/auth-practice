ea39e234817720ce1450b38280b023e089174e54
Arbaz Khan <83135990+ArbazKhan-Developer@users.noreply.github.com>
2023-06-02 15:05:54 +0530

    Update authApiProcess.js

--- api/src/authApiProcess.js
+++ api/src/authApiProcess.js
@@ -3,7 +3,6 @@ class authApiProcess {
 
     async process(event){
         console.log(`event received: ${JSON.stringify(event)}`);
-
         return {
             statuCode: 200,
             message: 'api processed successfully'
