301b77e6963f6d055d2e6b1b248a0974ff2da1c7
Arbaz Khan <83135990+ArbazKhan-Developer@users.noreply.github.com>
2023-06-02 16:58:12 +0530

    Update auth-handler.js

--- serverless/handlers/src/auth-handler.js
+++ serverless/handlers/src/auth-handler.js
@@ -2,9 +2,12 @@
 // const apiProcess = require('../../../api/src/authApiProcess')
 module.exports.hello = async (event) => {
     
+  console.log(`event received:: ${JSON.stringify(event)}`);
+  console.log('returning the response');
+
   return {
-      statusCode: 200,
-      Message: 'api processed successpully'
+    statusCode: 200,
+    body: 'api processed successfully'
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
