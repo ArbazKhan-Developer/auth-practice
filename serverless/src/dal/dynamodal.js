ff4cce610e64f8ec7277fa5b2703eeb3f1d340e0
Arbaz Khan <83135990+ArbazKhan-Developer@users.noreply.github.com>
2023-06-03 23:50:43 +0530

    Add files via upload
    done

--- serverless/src/dal/dynamodal.js
+++ serverless/src/dal/dynamodal.js
@@ -1,9 +1,9 @@
 const AWS = require('aws-sdk')
-AWS.config.update({region: 'us-east-1'});
-const docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+AWS.config.update({ region: 'us-east-1' });
+const docClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 const tableName = process.env.TABLE_NAME
 
-module.exports.insertData = async (body)=>{
+module.exports.insertData = async (body) => {
 
     try {
         console.log(`tableName: ${JSON.stringify(tableName)}`);
@@ -11,16 +11,34 @@ module.exports.insertData = async (body)=>{
             TableName: tableName,
             Item: JSON.parse(body),
             ReturnValues: 'ALL_OLD'
-          };
-         console.log(params)
-         const res = await docClient.put(params).promise()
-         console.log(`data inserted successfully: ${JSON.stringify(res)}`)
-         return 'data inserted successfully! Heart eyes'
-               
+        };
+        console.log(params)
+        const res = await docClient.put(params).promise()
+        console.log(`data inserted successfully: ${JSON.stringify(res)}`)
+        return 'data inserted successfully! Heart eyes'
+
     } catch (error) {
         console.log(`error occourred while inserting data into dynamo`);
         console.log(error);
         throw error
+    }
+
+
+}
+
+module.exports.getItem = async (body) => {
+
+    try {
+        var params = {
+            TableName: tableName,
+            Key: JSON.parse(body) 
+        };
+        const res = await docClient.get(params).promise()
+        console.log(`data inserted successfully: ${JSON.stringify(res)}`)
+        return 'data fetched successfully! Heart eyes'
+
+    } catch (error) {
+        console.log(error);
     }
 
 
