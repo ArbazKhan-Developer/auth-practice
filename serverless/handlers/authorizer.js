88cd62688f3374ed0b508930af3bd5ae9f80ec64
ArbazKhan-Developer <kkabz860156@gmail.com>
2023-07-08 19:57:59 +0530

    custome authorizer added

--- serverless/handlers/authorizer.js
+++ serverless/handlers/authorizer.js
@@ -1,16 +1,38 @@
-'use strict';
-const apiProcess = require('../src/api/authApi')
+"use strict";
+// const apiProcess = require('../src/api/authApi')
 module.exports.auth = async (event) => {
-    
   console.log(`event received:: ${JSON.stringify(event)}`);
-  console.log('returning the response');
-  const res = await apiProcess.authProcess(event)
+  console.log(`methodArn: ${event.methodArn}`);
+  if (event.headers["my-token"]) {
+    const token = process.env.TOKEN;
+    console.log(`token: ${token}`);
+    const myToken = event.headers["my-token"];
 
-  return {
-    statusCode: res.statusCode,
-    body: res.message
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    if (token === myToken) {
+      return await callResponseFunction("my-auth-api", "Allow", event.methodArn);
+    } else {
+      return await callResponseFunction("my-auth-api", "Deny", event.methodArn);
+    }
+  } else {
+    return {
+      statusCode: 400,
+      body: "unAuthorized access",
+    };
+  }
 };
+
+async function callResponseFunction(principalId, effect, resource) {
+  return {
+    principalId: principalId,
+    policyDocument: {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: "execute-api:Invoke",
+          Effect: effect,
+          Resource: resource,
+        },
+      ],
+    },
+  };
+}
--- serverless/src/api/authApi.js
+++ serverless/src/api/authApi.js
@@ -3,7 +3,7 @@ const requestModal = require('../modal/requestModal')
 
 class authApi{
    static async authProcess(event){
-    // console.log(`event received:: ${JSON.stringify(event)}`);
+    
     const apiKey = '52c7edad36ff27f9b0f5f7932ffe0af8'
     if (event.headers['x-api-key'] && event.headers['x-api-key'] == apiKey) {
         console.log('authenticating api key');
